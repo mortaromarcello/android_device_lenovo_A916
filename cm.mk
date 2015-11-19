6117b8d6c371f4eee4df662c52b0bb3325cb337d
Marcello Mortaro <mortaromarcello@gmail.com>
2015-11-19 11:14:53 +0100

    modified:   cm.mk 	modified:   rootdir/recovery.fstab 	modified:   rootdir/twrp.fstab 	modified:   system.prop

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := K1_turbo
+PRODUCT_RELEASE_NAME := A916
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/kingzone/k1_turbo/device_k1-turbo.mk)
+$(call inherit-product, device/lenovo/A916/device_A916.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := k1_turbo
-PRODUCT_NAME := cm_k1_turbo
-PRODUCT_BRAND := KINGZONE
-PRODUCT_MODEL := K1_turbo
-PRODUCT_MANUFACTURER := KINGZONE
+PRODUCT_DEVICE := A916
+PRODUCT_NAME := cm_A916
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := A916
+PRODUCT_MANUFACTURER := lenovo
